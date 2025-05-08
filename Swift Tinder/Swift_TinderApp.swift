bea2f7f6a1df2b586604575449d033e7288a9819
phuongdq <dqphuong2k2@gmail.com>
2025-05-08 14:13:47 +0700

    Initial Commit

---
+++ Swift Tinder/Swift_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TinderApp.swift
+//  Swift Tinder
+//
+//  Created by Duong Phuong on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
